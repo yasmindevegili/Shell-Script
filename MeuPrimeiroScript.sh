23052e73afb754e688d135cd6e0bf6cd9fecafec
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2023-02-28 16:29:20 -0300

    Create MeuPrimeiroScript.sh

---
+++ MeuPrimeiroScript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+  
+############################ 
+# 
+#  Meu Primeiro Script 
+#  Autor: Yasmin Devegili 
+#  Data: 22/02/2003 
+#  Script de imprimir data 
+#  e ler outros arquivo 
+# 
+############################ 
+  
+echo " " 
+echo "======================" 
+echo -n  "Meu Primeiro Script" 
+echo " " 
+date 
+echo "======================" 
+echo " " 
+echo "Listagem de alunos: " 
+sort arquivo.txt 
+echo " " 
+echo "Fim do Script" 
+ 
